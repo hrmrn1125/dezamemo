927813a64455fc6dcfbe74cb19089a4fc1152919
hara <hm.hzm331@gmail.com>
2024-06-06 06:03:13 +0900

    js

--- assets/js/index.js
+++ assets/js/index.js
@@ -54,3 +54,23 @@ btn8.addEventListener("click",function(){
   list8.classList.toggle("hidden");
   btn8.classList.toggle("close");
 });
+
+const targets = document.getElementsByClassName('fade');
+for(let i = targets.length; i--;){
+  let observer = new IntersectionObserver((entries, observer) => {
+  for(let j = entries.length; j--;){
+    if (entries[j].isIntersecting) {
+      entries[j].target.classList.add('active');
+      observer.unobserve(entries[j].target);
+    }
+  }
+  });
+  observer.observe(targets[i]);
+}
+
+const pagetop_btn = document.querySelector(".p-pageTop");
+
+pagetop_btn.addEventListener("click", scroll_top);
+function scroll_top() {
+  window.scroll({ top: 0, behavior: "smooth" });
+}
